729ea105531f82e6f85692714cc8f2127bf84cd5
shadinaguib <shadinaguib98@gmail.com>
2026-01-04 18:03:50 -0500

    Initial Commit

---
+++ RoutineTimers/RoutineTimersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoutineTimersApp.swift
+//  RoutineTimers
+//
+//  Created by Shadi Naguib on 1/4/26.
+//
+
+import SwiftUI
+
+@main
+struct RoutineTimersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
